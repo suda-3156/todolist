67c017a58942ffd7783e2207a9377c3d1a70354d
suda-3156win <sudasu0109@gmail.com>
2024-09-17 19:58:28 +0900

    240917 readme.md

---
+++ README.md
@@ -0,0 +1,33 @@
+# Todolist
+
+## 使い方
+
+* mysqlの設定
+
+`git clone`したのち，
+
+```
+mkdir ./mysql/logs && cd $_
+touch mysql-error.log mysql-query.log mysql-slow.log
+```
+
+を実行する．
+
+* compose.ymlの設定
+
+ルートディレクトリで`touch .env`したのち，以下を書き入れる．
+
+```
+ROOT_PASSWORD=パスワード
+DB_NAME=データベース名
+DB_USER=開発で使うユーザー
+DB_PASS=開発で使うユーザーのパスワード
+TZ=Asia/Tokyo
+```
+
+* node.jsの設定
+
+とくになし．
+
+### 初回起動
+
--- server/01_repository/TodolistRepository.ts
+++ server/01_repository/TodolistRepository.ts
@@ -38,7 +38,7 @@ export interface ITodolistRepository {
   // user_idでtodolist_attrs_listを返す
   findByUserId: (user_id: string, skip:number, take: number) => Promise<Todolist_attrs[]> 
   // todolist_idでtodolist_attrsとtodosを指定されたoffset, lengthで見つける
-  getTodos: (todolist_id: string, skip: number, take: number) => Promise<Todo[]>
+  getTolist: (todolist_id: string, skip: number, take: number) => Promise<Todo[]>
   // todo_idでtodoを見つける
   findByTodoId: (todo_id: string) => Promise<Todo>
 
@@ -95,7 +95,7 @@ export class TodolistRepository implements ITodolistRepository {
     return todolist_attrs_list
   }
 
-  getTodos = async (todolist_id: string, skip: number, take: number ) :Promise<Todolist_attrs> => {
+  getTodolist = async (todolist_id: string, skip: number, take: number ) :Promise<Todolist_attrs> => {
     const todolist_attrs = await this.prisma.user_todolist.findUnique({
       where: { todolist_id: todolist_id },
       select: {
@@ -114,120 +114,120 @@ export class TodolistRepository implements ITodolistRepository {
     return todolist_attrs
   }
 
-  getTodos = async (todolist_id: string, skip: number, take: number) :Promise<Todo[]> => {
-    const todo_ids = await this.prisma.todolist_todo.findMany({
-      skip: skip,
-      take: take,
-      where: { todolist_id: todolist_id },
-      select: {
-        todo_id: true,
-        todo_title: true,
-        updatedAt: true,
-      },
-      orderBy: {
-        updatedAt: 'desc'
-      }
-    })
+  // getTodos = async (todolist_id: string, skip: number, take: number) :Promise<Todo[]> => {
+  //   const todo_ids = await this.prisma.todolist_todo.findMany({
+  //     skip: skip,
+  //     take: take,
+  //     where: { todolist_id: todolist_id },
+  //     select: {
+  //       todo_id: true,
+  //       todo_title: true,
+  //       updatedAt: true,
+  //     },
+  //     orderBy: {
+  //       updatedAt: 'desc'
+  //     }
+  //   })
 
-    const todos = await Promise.all(todo_ids.map( async (todo_attrs) => {
-      const todo = await this.prisma.todo_info.findUnique({
-        where: { todo_id: todo_attrs.todo_id }
-      })
-      if( !todo ) {
-        throw new TodolistRepositoryError
-      }
-      return {
-        todo_id: todo_attrs.todo_id,
-        todo_title: todo_attrs.todo_title,
-        completed: todo.completed,
-        deleted: todo.deleted,
-        style: todo.style,
-        updatedAt: todo.updatedAt
-      }
-    }))
+  //   const todos = await Promise.all(todo_ids.map( async (todo_attrs) => {
+  //     const todo = await this.prisma.todo_info.findUnique({
+  //       where: { todo_id: todo_attrs.todo_id }
+  //     })
+  //     if( !todo ) {
+  //       throw new TodolistRepositoryError
+  //     }
+  //     return {
+  //       todo_id: todo_attrs.todo_id,
+  //       todo_title: todo_attrs.todo_title,
+  //       completed: todo.completed,
+  //       deleted: todo.deleted,
+  //       style: todo.style,
+  //       updatedAt: todo.updatedAt
+  //     }
+  //   }))
 
-    return todos
-  }
+  //   return todos
+  // }
 
-  findByTodoId = async (todo_id: string) :Promise<Todo> => {
-    const todo_attrs = await this.prisma.todolist_todo.findUnique({
-      where: { todo_id: todo_id },
-      select: {
-        todo_id: true,
-        todo_title: true,
-        updatedAt: true,
-      }
-    })
-    if( !todo_attrs ) {
-      throw new TodolistRepositoryError
-    }
+  // findByTodoId = async (todo_id: string) :Promise<Todo> => {
+  //   const todo_attrs = await this.prisma.todolist_todo.findUnique({
+  //     where: { todo_id: todo_id },
+  //     select: {
+  //       todo_id: true,
+  //       todo_title: true,
+  //       updatedAt: true,
+  //     }
+  //   })
+  //   if( !todo_attrs ) {
+  //     throw new TodolistRepositoryError
+  //   }
 
-    const todo = await this.prisma.todo_info.findUnique({
-      where: { todo_id: todo_id }
-    })
-    if ( !todo ) {
-      throw new TodolistRepositoryError
-    }
+  //   const todo = await this.prisma.todo_info.findUnique({
+  //     where: { todo_id: todo_id }
+  //   })
+  //   if ( !todo ) {
+  //     throw new TodolistRepositoryError
+  //   }
 
-    return todo
-  }
+  //   return todo
+  // }
 
-  upsertTodolist = async (todolist: Todolist_attrs) :Promise<Todolist_attrs> => {
-    const todolist_attrs = await this.prisma.user_todolist.upsert({
-      where: { todolist_id: todolist.todolist_id },
-      create: {
-        todolist_id: todolist.todolist_id,
-        todolist_title: todolist.todolist_title,
-        style: todolist.style,
-        user_id: todolist.user_id,
-      },
-      update: {
-        todolist_title: todolist.todolist_title,
-        style: todolist.style
-      }
-    })
-    return todolist_attrs
-  }
+  // upsertTodolist = async (todolist: Todolist_attrs) :Promise<Todolist_attrs> => {
+  //   const todolist_attrs = await this.prisma.user_todolist.upsert({
+  //     where: { todolist_id: todolist.todolist_id },
+  //     create: {
+  //       todolist_id: todolist.todolist_id,
+  //       todolist_title: todolist.todolist_title,
+  //       style: todolist.style,
+  //       user_id: todolist.user_id,
+  //     },
+  //     update: {
+  //       todolist_title: todolist.todolist_title,
+  //       style: todolist.style
+  //     }
+  //   })
+  //   return todolist_attrs
+  // }
 
-  deleteTodolist = async (todolist_id: string) :Promise<Todolist_attrs> => {
+  // deleteTodolist = async (todolist_id: string) :Promise<Todolist_attrs> => {
     
-    const todo_attrs_list = await this.prisma.todolist_todo.findMany({
-      where: { todolist_id: todolist_id }
-    })
-    if ( !todo_attrs_list ) {
-      throw new TodolistRepositoryError
-    }
+  //   const todo_attrs_list = await this.prisma.todolist_todo.findMany({
+  //     where: { todolist_id: todolist_id }
+  //   })
+  //   if ( !todo_attrs_list ) {
+  //     throw new TodolistRepositoryError
+  //   }
 
-    await this.prisma.$transaction([
+  //   await this.prisma.$transaction([
 
-    ])
+  //   ])
 
-    await Promise.all(
-      todo_attrs_list.map( async (todo_attrs) => {
-        await this.prisma.todo_info.delete({
-          where: { todo_id: todo_attrs.todo_id }
-        }).catch (() => {
-          throw new Error
-        })
-      })
-    )
+  //   await Promise.all(
+  //     todo_attrs_list.map( async (todo_attrs) => {
+  //       await this.prisma.todo_info.delete({
+  //         where: { todo_id: todo_attrs.todo_id }
+  //       }).catch (() => {
+  //         throw new Error
+  //       })
+  //     })
+  //   )
 
-    await this.prisma.todolist_todo.deleteMany({
-      where: { todolist_id: todolist_id}
-    }).catch(() => {
-      throw new TodolistRepositoryError
-    })
+  //   await this.prisma.todolist_todo.deleteMany({
+  //     where: { todolist_id: todolist_id}
+  //   }).catch(() => {
+  //     throw new TodolistRepositoryError
+  //   })
     
-    await this.prisma.user_todolist.delete({
-      where: { todolist_id: todolist_id }
-    }).catch(() => {
-      throw new TodolistRepositoryError
-    })
-  }
-  // todo_idでupsertする
-  upsertTodo: (todo: Todo) => Promise<Todo>
-  // todo_idでdeleteする
-  deleteTodo: (todo_id: string) => Promise<Todo>
+  //   await this.prisma.user_todolist.delete({
+  //     where: { todolist_id: todolist_id }
+  //   }).catch(() => {
+  //     throw new TodolistRepositoryError
+  //   })
+  // }
+  // // todo_idでupsertする
+  // upsertTodo: (todo: Todo) => Promise<Todo>
+  // // todo_idでdeleteする
+  // deleteTodo: (todo_id: string) => Promise<Todo>
 }
 
 //TODO: db初期化する
